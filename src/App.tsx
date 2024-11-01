74838694105021e6761cf4af0fe9751d0177f848
Rob Doneux <robertdoneux@gmail.com>
2024-11-01 08:01:06 +0000

    adds footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import Home from './routes/home/Home';
 import NotFound from './routes/not-found/NotFound';
 import Login from './routes/login/Login';
 import { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div className="w-full flex align-top justify-center grow">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
       <Toaster toastOptions={{ style: { backgroundColor: '#d4d4d4' } }} />
     </BrowserRouter>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import {version} from '../../package.json';
+
+export default function Footer() {
+  return (
+    <footer className="w-full px-2">
+        <ul className="grid grid-cols-3 text-sm">
+            <li className="col-start-2 justify-self-center">{process.env.VITE_PROJECT_NAME}</li>
+            <li className="justify-self-end">{version}</li>
+        </ul>
+    </footer>
+  )
+}
