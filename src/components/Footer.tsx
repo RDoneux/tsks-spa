1e96847dbeba785be0d7d5f4eba811bccfe69464
Rob Doneux <robertdoneux@gmail.com>
2024-11-01 08:06:08 +0000

    runs prettier

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,14 @@
-import {version} from '../../package.json';
+import { version } from '../../package.json';
 
 export default function Footer() {
   return (
     <footer className="w-full px-5 md:px-2">
-        <ul className="grid grid-cols-3 text-sm">
-            <li className="col-start-2 justify-self-center">{process.env.VITE_PROJECT_NAME}</li>
-            <li className="justify-self-end">{version}</li>
-        </ul>
+      <ul className="grid grid-cols-3 text-sm">
+        <li className="col-start-2 justify-self-center">
+          {process.env.VITE_PROJECT_NAME}
+        </li>
+        <li className="justify-self-end">{version}</li>
+      </ul>
     </footer>
-  )
+  );
 }
